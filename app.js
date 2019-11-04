33dec619d71978fde6611d4a24498a610bbd2fed
Orenmc <orenmc.c90@gmail.com>
2019-11-04 19:41:08 +0200

    some files order

--- app.js
+++ app.js
@@ -7,4 +7,5 @@ const routes = require('./routes');
 const server = http.createServer(routes.handler);
 // loop: keep running and listen 
 
+console.log('server is ON');
 server.listen(3000); // port - 3000
