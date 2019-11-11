867d21671795b2c53ea433e6b120ed6f5b7166a0
Orenmc <orenmc.c90@gmail.com>
2019-11-11 17:30:21 +0200

    get the routes logic out to another folder

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -1,30 +1,29 @@
-const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
-
-
+/*
 app.get('/favicon.ico', (req, res) => {
     res.status(204);
-});
+}); //good for  dealing with entering twice (reload twice) the page
+*/
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: true}));   //parsing the request (ends from form!)
 
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
-}); // next - is a function!, must be excuted
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
+// should be last- default catch
+app.use((req,res,next) =>{
+    res.status(404).send('<h1>page not found</h1>');
 });
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!1</h1><h2>tring to do something else</h2>');
-}); // next - is a function!, must be excuted
 
 
-console.log('listen');
+//open server and listen on port 3000
+console.log('listening...');
 app.listen(3000);
 
